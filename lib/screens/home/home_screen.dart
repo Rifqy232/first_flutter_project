06879d91e3cf9718efc0fd8eb8cc9c18592dc057
Rifqy232 <rachmanmry20d@student.unhas.ac.id>
2023-01-24 21:34:11 +0800

    First commit

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+int _counter = 0;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("First App"),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF075e54),
+        leading: const Icon(
+          Icons.menu_rounded,
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+            ),
+            child: Icon(
+              Icons.person,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$_counter",
+              style: const TextStyle(
+                fontSize: 52,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _counter++;
+                    });
+                  },
+                  child: const Text(
+                    "+",
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _counter--;
+                    });
+                  },
+                  child: const Text(
+                    "-",
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
